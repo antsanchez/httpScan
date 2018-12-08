418d97290c5796dc5a9d6d989a353f8cdbc9d576
Antonio Sánchez <antsanchez.dev@gmail.com>
2018-12-08 13:38:09 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# httpScan
+
+httpScan is a very simple CLI app which accepts a URL and print some Meta data from it, like the headers or the time the URL took to respond.
+
+To execute, just type in your CLI: httpScan http://domain-to-scan.com
+
+### Download and Install
+
+#### Binary Distributions
+
+You can download the binaries for Linux, Windows and Mac that you will find under the binaries folder. 
+
+#### Install From Source
+
+Just download and compile it as any other standard Go software. More info about Go: https://golang.org/
---
+++ main.go
@@ -0,0 +1,82 @@
+// Copyright 2018 Antonio Sanchez. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/olekukonko/tablewriter"
+)
+
+func main() {
+	doAndPrintRequest(os.Args[1])
+}
+
+func doAndPrintRequest(url string) {
+
+	start := time.Now()
+
+	res, err := http.Get(url)
+	check(err)
+
+	elapsed := time.Now().Sub(start)
+
+	defer res.Body.Close()
+
+	fmt.Println("")
+
+	// Request Info
+	table := tablewriter.NewWriter(os.Stdout)
+	table.Append([]string{"URL", url})
+	table.Append([]string{"Time", elapsed.String()})
+	table.Append([]string{"Status", res.Status})
+	table.Append([]string{"Protocol", res.Proto})
+	table.Append([]string{"Compressed", strconv.FormatBool(res.Uncompressed)})
+	table.SetHeader([]string{"Action / Header", "Value"})
+
+	// Header Info
+	setcookie := ""
+	for i, val := range res.Header {
+		if strings.Compare(i, "Set-Cookie") == 0 {
+			setcookie = strings.Join(val, " ")
+		} else {
+			table.Append([]string{i, strings.Join(val, " ")})
+		}
+	}
+
+	table.SetHeader([]string{"Header", "Value"})
+	table.SetRowLine(true)
+	table.Render()
+
+	// Set-Cookie value ill be show separately,
+	// since it is often too long and breaks the table
+	if strings.Compare(setcookie, "") != 0 {
+		fmt.Println("Set-Cookie:")
+		fmt.Println(setcookie)
+	}
+
+	fmt.Println("")
+}
+
+func check(err error) {
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
